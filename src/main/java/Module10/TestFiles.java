b8832feeafc6bf4bb8208dd13843aa246d541a82
EugeneDnipro <eugenednepr@gmail.com>
2022-12-26 16:59:54 +0200

    In WorkWithFiles class added dataFromFileReader and wordCounter methods. And made changes in phoneFileValidate method.

--- src/main/java/Module10/TestFiles.java
+++ src/main/java/Module10/TestFiles.java
@@ -3,6 +3,8 @@ package Module10;
 public class TestFiles {
     public static void main(String[] args) {
         WorkWithFiles.phoneFileValidate();
+        System.out.println("==================");
+        WorkWithFiles.wordCounter();
     }
 
 }
--- src/main/java/Module10/WorkWithFiles.java
+++ src/main/java/Module10/WorkWithFiles.java
@@ -2,17 +2,17 @@ package Module10;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WorkWithFiles {
-    public static void phoneFileValidate() {
+
+    public static char[] dataFromFileReader(String path) {
         char[] buf = new char[256];
-        try (FileReader reader = new FileReader(".\\src\\main\\resources\\file.txt")) {
+        int count = 0;
+        try (FileReader reader = new FileReader(path)) {
             int c;
-            int count = 0;
             while ((c = reader.read(buf)) > 0) {
                 count++;
                 if (c < 256) {
@@ -25,21 +25,34 @@ public class WorkWithFiles {
             buf[buf.length - 1] = '\n';
         } catch (IOException e) {
             System.out.println(e.getMessage());
+            return null;
+        }
+        return buf;
+    }
+
+    public static void phoneFileValidate() {
+        char[] buf;
+        String path = ".\\src\\main\\resources\\file.txt";
+        if (dataFromFileReader(path) == null) {
             return;
+        } else {
+            buf = dataFromFileReader(path);
         }
 
         StringBuilder result = new StringBuilder();
         ArrayList<String> numbers = new ArrayList<String>();
         int count = 0;
-        for (char item : buf) {
-            if ((item != '\r') && (item != '\n')) {
-                result.append(item);
-                count++;
-            } else if (count > 0) {
-                String number = result.toString();
-                result.delete(0, result.length());
-                numbers.add(number);
-                count = 0;
+        if (buf != null) {
+            for (char item : buf) {
+                if ((item != '\r') && (item != '\n')) {
+                    result.append(item);
+                    count++;
+                } else if (count > 0) {
+                    String number = result.toString();
+                    result.delete(0, result.length());
+                    numbers.add(number);
+                    count = 0;
+                }
             }
         }
 
@@ -50,8 +63,58 @@ public class WorkWithFiles {
         for (String number : numbers) {
             Matcher matcher = pattern.matcher(number);
             if (matcher.matches()) {
-                System.out.println("number = " + number);
+                System.out.println(number);
             }
         }
+    }
+
+    public static void wordCounter() {
+        char[] buf;
+        String path = ".\\src\\main\\resources\\words.txt";
+        if (dataFromFileReader(path) == null) {
+            return;
+        } else {
+            buf = dataFromFileReader(path);
+        }
+
+        StringBuilder result = new StringBuilder();
+        ArrayList<String> words = new ArrayList<String>();
+        int count = 0;
+        if (buf != null) {
+            for (char item : buf) {
+                if ((item != '\r') && (item != '\n') && (item != ' ')) {
+                    result.append(item);
+                    count++;
+                } else if (count > 0) {
+                    String word = result.toString();
+                    result.delete(0, result.length());
+                    words.add(word);
+                    count = 0;
+                }
+            }
+        }
+
+        Map<String, Integer> wordsNumber = new HashMap<>();
+        for (String elem : words) {
+            if (wordsNumber.containsKey(elem))
+                wordsNumber.put(elem, wordsNumber.get(elem) + 1);
+            else wordsNumber.put(elem, 1);
+        }
+
+        List<Map.Entry<String, Integer>> wordsNumberList = new ArrayList(wordsNumber.entrySet());
+        wordsNumberList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+                return b.getValue() - a.getValue();
+            }
+        });
+
+        Map<String, Integer> sortedWordsNumber = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : wordsNumberList) {
+            sortedWordsNumber.put(entry.getKey(), entry.getValue());
+        }
+
+        for (Map.Entry<String, Integer> entry : sortedWordsNumber.entrySet())
+            System.out.println(entry.getKey() + " " + entry.getValue());
     }
 }
