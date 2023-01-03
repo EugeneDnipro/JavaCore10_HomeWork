99d94401577115f2bd0380e0fba2c0555cfedd05
EugeneDnipro <eugenednepr@gmail.com>
2023-01-03 12:50:06 +0200

    Added TimeCount and TestParallelStreams classes in Module12.

---
+++ src/main/java/Module12/TestParallelStreams.java
@@ -0,0 +1,8 @@
+package Module12;
+
+public class TestParallelStreams {
+    public static void main(String[] args) {
+        TimeCount timer = new TimeCount();
+        timer.beep();
+    }
+}
---
+++ src/main/java/Module12/TimeCount.java
@@ -0,0 +1,26 @@
+package Module12;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class TimeCount {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
+    long start = System.currentTimeMillis();
+
+    public void beep() {
+        final Runnable beeperFive = () -> System.out.println("Минуло 5 секунд");
+
+        final Runnable beeperOne = () -> System.out.println((System.currentTimeMillis() - start) / 1000);
+
+        final ScheduledFuture<?> beeperHandleOne = scheduler.scheduleAtFixedRate(beeperOne, 1, 1, SECONDS);
+        final ScheduledFuture<?> beeperHandleFive = scheduler.scheduleAtFixedRate(beeperFive, 5, 5, SECONDS);
+
+        scheduler.schedule(() -> {
+            beeperHandleOne.cancel(true);
+            beeperHandleFive.cancel(true);
+        }, 30, SECONDS);
+    }
+}
